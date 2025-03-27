b06f7554555ef3e26317f69564886b603b0ae86b
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-03-27 12:11:26 +0600

    1006 - Average 2

---
+++ Average_2.py
@@ -0,0 +1,10 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+weight_A = 2
+weight_B = 3
+weight_C = 5
+
+average = (A*weight_A + B*weight_B + C*weight_C) / (weight_A + weight_B + weight_C)
+print(f"MEDIA = {average:.1f}")
