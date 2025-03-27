c6e1bcc0ad3128794e4886305dbe613511ebd430
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-03-27 12:57:20 +0600

    1009 - Salary with Bonus

---
+++ Salary_with_Bonus.py
@@ -0,0 +1,7 @@
+seller_name = str(input())
+fixed_salary = float(input())
+total_sales = float(input())
+
+total_salary = fixed_salary + (total_sales * 0.15)
+
+print(f"TOTAL = R$ {total_salary:.2f}")
