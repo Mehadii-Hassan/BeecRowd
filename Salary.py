a8d7771b6b5b8e4ed298b45cc7b762e019a909b0
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-03-27 12:41:53 +0600

    1008 - Salary

---
+++ Salary.py
@@ -0,0 +1,8 @@
+employee_number = int(input())
+worked_hours = int(input())
+hour_rate = float(input())
+
+salary = worked_hours * hour_rate
+
+print(f"NUMBER = {employee_number}")
+print(f"SALARY = U$ {salary:.2f}")
